13c5878d8376e7eaaad39127dbafe1788b202a8c
yassin <17247519+Yavanha@users.noreply.github.com>
2025-05-06 14:30:45 +0200

    feat: add some terminal info

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from maze import Maze
 
 
 def main():
+    print("Initializing the Maze solver game....")
     num_rows = 16
     num_cols = 16
     margin = 50
@@ -11,10 +12,24 @@ def main():
     screen_y = 800
     cell_size_x = (screen_x - 2 * margin) / num_cols
     cell_size_y = (screen_y - 2 * margin) / num_rows
+    print(f"""
+Configuration : 
+    - Screen resolution : {screen_x} x {screen_y}
+    - Game matrix : {num_rows} x {num_cols}
+    - Cell's width : {cell_size_x}
+    - Cell's height : {cell_size_y}
+""")
     win = Window(screen_x, screen_y)
+    print("Game board created...")
     maze = Maze(margin, margin, num_rows, num_cols,
                 cell_size_x, cell_size_y, win)
-    maze.solve()
+    print("Maze created...")
+    print("resolving....")
+    resolved = maze.solve()
+    if resolved:
+        print("The maze has been resolved!")
+    else:
+        print("The maze can't be resolve!")
     win.wait_for_close()
 
 
